abce581d7a3777ddbf581cb53e7eea330a253710
Markus Klein <markus-klein@live.de>
2020-06-06 00:01:53 +0200

    sampling

--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,47 @@ mod ray;
 mod sphere;
 mod vec3;
 
-use camera::Camera;
-use hittable::Hittable;
-use ray::Ray;
-use sphere::Sphere;
+use crate::{
+    camera::Camera,
+    hittable::Hittable,
+    ray::Ray,
+    sphere::Sphere,
+    vec3::{Color, Point},
+};
+use rand::{thread_rng, Rng};
 use std::io;
-use vec3::{Color, Point};
 
 fn main() -> io::Result<()> {
     let aspect_ratio = 16. / 9.;
     let image_width = 384;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
+    let samples_per_pixel = 100;
 
     // P3 means colors are in ASCII. Then width, then height. 255 is the max color.
     print!("P3\n{} {}\n255\n", image_width, image_height);
-
-    let camera = Camera::new(aspect_ratio);
 
     let world = vec![
         Sphere::new(Point::new(0., 0., -1.), 0.5),
         Sphere::new(Point::new(0., -100.5, -1.), 100.),
     ];
 
+    let camera = Camera::new(aspect_ratio);
+
+    let mut rng = thread_rng();
+
     for j in (0..image_height).into_iter().rev() {
         eprintln!("Scanlines remaining: {}", j);
         for i in 0..image_width {
-            let u = i as f64 / (image_width - 1) as f64;
-            let v = j as f64 / (image_height - 1) as f64;
-            let ray = camera.get_ray(u, v);
-            let pixel_color = ray_color(&ray, &world);
+            let acc_color = (0..samples_per_pixel)
+                .into_iter()
+                .map(|_| {
+                    let u = (i as f64 + rng.gen_range(0., 1.)) / (image_width - 1) as f64;
+                    let v = (j as f64 + rng.gen_range(0., 1.)) / (image_height - 1) as f64;
+                    let ray = camera.get_ray(u, v);
+                    ray_color(&ray, &world)
+                })
+                .fold(Color::new(0., 0., 0.), |a, b| a + b);
+            let pixel_color = acc_color / samples_per_pixel as f64;
             write_color(&pixel_color, io::stdout().lock())?;
         }
     }
@@ -43,9 +55,9 @@ fn main() -> io::Result<()> {
 }
 
 pub fn write_color(color: &Color, mut out: impl io::Write) -> io::Result<()> {
-    let red = (255.999 * color[0]) as i32;
-    let green = (255.999 * color[1]) as i32;
-    let blue = (255.999 * color[2]) as i32;
+    let red = (256. * clamp(color[0], 0., 0.999)) as i32;
+    let green = (256. * clamp(color[1], 0., 0.999)) as i32;
+    let blue = (256. * clamp(color[2], 0., 0.999)) as i32;
 
     // Now print RGB tripplets
     writeln!(out, "{} {} {}", red, green, blue)
@@ -62,5 +74,15 @@ fn ray_color(ray: &Ray, world: &impl Hittable) -> Color {
         let blend_start = Color::new(1., 1., 1.);
         let blend_end = Color::new(0.5, 0.7, 1.0);
         blend_start * (1. - t) + blend_end * t
+    }
+}
+
+fn clamp(f: f64, low: f64, high: f64) -> f64 {
+    if f < low {
+        low
+    } else if f > high {
+        high
+    } else {
+        f
     }
 }

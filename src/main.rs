d992b6ea3de200d4f4f6fcc0858d6ba39a1c48b6
Markus Klein <markus-klein@live.de>
2020-06-15 19:18:34 +0200

    support cancelling with Ctrl+C

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,16 @@ use crate::{
 use indicatif::{ProgressBar, ProgressStyle};
 use rand::{rngs::ThreadRng, thread_rng, Rng};
 use rayon::prelude::*;
-use std::{io, path::PathBuf};
 use structopt::StructOpt;
+
+use std::{
+    io,
+    path::PathBuf,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
 
 #[derive(StructOpt)]
 struct Cli {
@@ -36,6 +44,16 @@ struct Cli {
 }
 
 fn main() -> io::Result<()> {
+    // Running is going to become `false` if execution is interrupted with Ctrl+C. In which case we
+    // want to stop rendering and produce output asap.
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
     let Cli {
         samples_per_pixel,
         max_depth,
@@ -64,41 +82,55 @@ fn main() -> io::Result<()> {
         0.1,
     );
 
-    let progress_bar = ProgressBar::new(samples_per_pixel as u64);
-    progress_bar.set_style(
-        ProgressStyle::default_bar().template("[{elapsed_precise}][{eta}] {bar:80.cyan/blue}  "),
+    eprintln!(
+        "Start rendering samples. You can press Ctrl+C to finish rendering the current samples and \
+        then produce output immediatly with samples rendered so far."
     );
 
-    let _pixels = (0..image_height)
-        .rev()
-        .flat_map(|j| (0..image_width).map(move |i| (i, j)));
+    let progress_bar = ProgressBar::new(samples_per_pixel as u64);
+    progress_bar.set_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}][{eta}] {wide_bar} samples: {pos}/{len}"),
+    );
 
-    let neutral = || vec![Color::new(0., 0., 0.); image_height as usize * image_width as usize];
-    let mut acc_color_buf: Vec<Color> = (0..samples_per_pixel)
+    let neutral = || {
+        (
+            vec![Color::new(0., 0., 0.); image_height as usize * image_width as usize],
+            0,
+        )
+    };
+    let (mut acc_color_buf, num_samples_rendered) = (0..samples_per_pixel)
         .into_par_iter()
         .map(|_| {
             let mut rng = thread_rng();
-            let sample = render_sample(
-                &mut rng,
-                max_depth,
-                image_height,
-                image_width,
-                &camera,
-                &world,
-            );
+            let sample = if running.load(Ordering::SeqCst) {
+                (
+                    render_sample(
+                        &mut rng,
+                        max_depth,
+                        image_height,
+                        image_width,
+                        &camera,
+                        &world,
+                    ),
+                    1,
+                )
+            } else {
+                neutral()
+            };
             progress_bar.inc(1);
             sample
         })
-        .reduce(neutral, |mut acc, sample| {
+        .reduce(neutral, |(mut acc, acc_num), (sample, weight)| {
             for index in 0..acc.len() {
                 acc[index] = acc[index] + sample[index];
             }
-            acc
+            (acc, acc_num + weight)
         });
 
     progress_bar.finish();
 
-    let samples_f = samples_per_pixel as f64;
+    let samples_f = num_samples_rendered as f64;
 
     for color in &mut acc_color_buf {
         *color = *color / samples_f;
